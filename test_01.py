b50e65ebdf3072dac9ade81973cc7d44bca6eeec
Anne An <anr@tradeshift.com>
2015-11-25 10:45:55 +0800

    save page data

--- test_01.py
+++ test_01.py
@@ -2,6 +2,7 @@
 import urllib
 import urllib2
 import re
+import os, errno
 
 baseUrl = 'http://www.xjtu.edu.cn/'
 urlList = [baseUrl]
@@ -33,8 +34,27 @@ def getURL(page):
 		else:
 			tempUrl = baseUrl + str(item)
 			tempList.append(tempUrl)
-	print tempList
 	return tempList
+
+def writeData(url):
+	file = None;
+	file = open(getFilePath(url), "w+")
+	file.write(getPage(url))
+
+def getFilePath(url):
+	dir = os.path.split(url)
+	if(dir[0] == 'http:'):
+		try:
+			os.makedirs(str(dir[1])	
+		except OSError as exc:
+			if exc.errno == errno.EEXIST and os.path.isdir(path):
+				pass
+			else: raise	
+		return str(dir[1]) + 'origin.htm'
+	else:
+		return ''
+
+
 
 
 def startCrawl(urlList):
@@ -46,9 +66,9 @@ def startCrawl(urlList):
 		crawledUrls.append(url)
 		print url,len(urlList)
 		temp = getURL(getPage(url))
-		print temp
 		temp.extend(urlList)
 		temp1 = list(set(temp))
 		startCrawl(temp1)
 
-startCrawl(urlList)
+#startCrawl(urlList)
+writeData('http://www.xjtu.edu.cn/')
